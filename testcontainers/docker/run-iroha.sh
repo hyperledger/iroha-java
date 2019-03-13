2171dc8b4c7c9b11a6ee06a86b910036c55d1728
Bogdan Vaneev <warchantua@gmail.com>
2019-03-13 14:04:51 +0200

    Add verbosity flag
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- testcontainers/docker/run-iroha.sh
+++ testcontainers/docker/run-iroha.sh
@@ -9,14 +9,15 @@ BLOCKS_PATH=${BLOCKS_PATH:-`cat ${CONFIG_PATH} | jq -r .block_store_path`}
 POSTGRES_HOST=${POSTGRES_HOST:-`cat ${CONFIG_PATH} | jq -r .pg_opt | cut -d' ' -f1 | cut -d'=' -f2`}
 WAIT_TIMEOUT=${WAIT_TIMEOUT:-10}
 KEY=${KEY}
+VERBOSITY=${VERBOSITY:-2}
 
 # postgres host is always defined
 /wait-for-it.sh -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -t ${WAIT_TIMEOUT} -- true
 
-if [ -z "$(ls -A ${BLOCKS_PATH})" ]; then
+if [[ -z "$(ls -A ${BLOCKS_PATH})" ]]; then
   # if ledger is empty then init ledger with genesis_block
-	irohad --genesis_block ${GENESISBLOCK_PATH} --config ${CONFIG_PATH} --keypair_name ${KEY}
+	irohad --genesis_block ${GENESISBLOCK_PATH} --config ${CONFIG_PATH} --keypair_name ${KEY} --verbosity ${VERBOSITY}
 else
   # else continue to run iroha without genesis_block
-	irohad --config ${CONFIG_PATH} --keypair_name ${KEY}
+	irohad --config ${CONFIG_PATH} --keypair_name ${KEY} --verbosity ${VERBOSITY}
 fi
