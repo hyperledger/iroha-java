8747c1c3ca8c7b652685b7ef0a483477aa3f6a73
Bogdan Mingela <freekilluau@gmail.com>
2019-05-22 18:41:21 +0300

    No (almost) limit for details value (#29)
    * No (almost) limit for details value
    
    Signed-off-by: Mingela <freekilluau@gmail.com>
    
    * fixed validator test
    
    Signed-off-by: Mingela <freekilluau@gmail.com>

--- client/src/main/java/jp/co/soramitsu/iroha/java/detail/Const.java
+++ client/src/main/java/jp/co/soramitsu/iroha/java/detail/Const.java
@@ -7,7 +7,7 @@ public class Const {
   public static final String assetIdDelimiter = "#";
   public static final String accountIdDelimiter = "@";
   public static final String hostPortDelimiter = ":";
-  public static final int accountDetailsMaxLength = 4096;
+  public static final int accountDetailsMaxLength = 4194304;
 
   public static final Pattern accountDetailsKeyPattern = Pattern.compile("[A-Za-z0-9_]{1,64}");
   public static final Pattern accountPattern = Pattern.compile("[a-z_0-9]{1,32}");
