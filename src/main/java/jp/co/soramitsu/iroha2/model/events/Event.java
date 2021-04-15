1beb2f14cba9876609fc9400fa107f1192578101
rkharisov <rinat@soramitsu.co.jp>
2021-04-15 09:58:25 +0300

    WIP
    Signed-off-by: rkharisov <rinat@soramitsu.co.jp>

--- src/main/java/jp/co/soramitsu/iroha2/model/events/Event.java
+++ src/main/java/jp/co/soramitsu/iroha2/model/events/Event.java
@@ -1,17 +1,5 @@
 package jp.co.soramitsu.iroha2.model.events;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-
-/**
- * Responses to event subscription.
- */
-@Data
-@NoArgsConstructor
-public class Event {
-
-  @NonNull
-  private EventType eventType;
+public interface EventType {
 
 }
--- src/main/java/jp/co/soramitsu/iroha2/model/events/EventType.java
+++
@@ -1,5 +0,0 @@
-package jp.co.soramitsu.iroha2.model.events;
-
-public interface EventType {
-
-}
