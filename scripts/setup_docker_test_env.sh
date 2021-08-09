53b5d0bce0d9a1a29a07ef134b908c3fe3fb7a48
rkharisov <50765419+rkharisov@users.noreply.github.com>
2021-08-09 16:27:59 +0300

    Resolved scale4j codec dependency (#89)
    Signed-off-by: rkharisov <rinat@soramitsu.co.jp>

--- scripts/setup_docker_test_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone https://github.com/hyperledger/iroha.git
-cd iroha || exit
-# specific version of iroha2 to test with
-git checkout iroha2-dev
-docker-compose -f docker-compose.yml up -d --build --force-recreate
-sleep 10
--- src/main/java/jp/co/soramitsu/iroha2/model/RejectedTransactionVariant.java
+++
@@ -1,5 +0,0 @@
-package jp.co.soramitsu.iroha2.model;
-
-public class RejectedTransactionVariant implements TransactionValueVariant {
-
-}
