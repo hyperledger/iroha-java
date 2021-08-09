4e37b66ec07be5b5f945d32924827b7f56eac65c
rkharisov <rinat@soramitsu.co.jp>
2021-08-09 16:19:20 +0300

    Resolved scale4j codec dependency

--- src/main/java/jp/co/soramitsu/iroha2/model/events/Data.java
+++
@@ -1,9 +0,0 @@
-package jp.co.soramitsu.iroha2.model.events;
-
-import lombok.NoArgsConstructor;
-
-@lombok.Data
-@NoArgsConstructor
-public class Data implements Event {
-
-}
--- src/main/java/jp/co/soramitsu/iroha2/model/expression/Expression.java
+++
@@ -1,7 +0,0 @@
-package jp.co.soramitsu.iroha2.model.expression;
-
-import jp.co.soramitsu.iroha2.model.Enumeration;
-
-public interface Expression extends Enumeration {
-
-}
