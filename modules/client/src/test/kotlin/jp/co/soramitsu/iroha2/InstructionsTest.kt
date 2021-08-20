8d7db218b70da7f54ed99b7b0c0f878b7f9329d7
rkharisov <50765419+rkharisov@users.noreply.github.com>
2021-08-20 14:30:37 +0300

    Improvements (#105)
    * Added grant permission token instruction
    
    Signed-off-by: rkharisov <rinat@soramitsu.co.jp>
    
    * Changed Iroha2 client to use ktor http client in instructions
    
    Signed-off-by: rkharisov <rinat@soramitsu.co.jp>
    
    * Removed OkHttp for websockets to use CIO
    
    Signed-off-by: rkharisov <rinat@soramitsu.co.jp>

--- modules/client/src/test/kotlin/jp/co/soramitsu/iroha2/InstructionsTest.kt
+++ modules/client/src/test/kotlin/jp/co/soramitsu/iroha2/InstructionsTest.kt
@@ -1,8 +1,8 @@
 package jp.co.soramitsu.iroha2
 
 import jp.co.soramitsu.iroha2.engine.ALICE_ACCOUNT_ID
+import jp.co.soramitsu.iroha2.engine.DEFAULT_DOMAIN_NAME
 import jp.co.soramitsu.iroha2.generated.datamodel.Value
-import jp.co.soramitsu.iroha2.generated.datamodel.account.Id
 import jp.co.soramitsu.iroha2.generated.datamodel.asset.AssetValue
 import jp.co.soramitsu.iroha2.generated.datamodel.asset.AssetValueType
 import jp.co.soramitsu.iroha2.generated.datamodel.asset.DefinitionId
@@ -19,6 +19,7 @@ import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 import kotlin.test.fail
 import jp.co.soramitsu.iroha2.generated.datamodel.asset.Id as AssetId
+import jp.co.soramitsu.iroha2.generated.datamodel.account.Id as AccountId
 
 @Execution(ExecutionMode.CONCURRENT)
 @Timeout(10)
@@ -42,7 +43,7 @@ class InstructionsTest {
 
     @Test
     fun `register account instruction committed`() {
-        val newAccountId = Id("foo", "wonderland")
+        val newAccountId = AccountId("foo", DEFAULT_DOMAIN_NAME)
         Assertions.assertDoesNotThrow {
             client.sendTransactionAsync {
                 accountId = ALICE_ACCOUNT_ID
@@ -59,7 +60,7 @@ class InstructionsTest {
 
     @Test
     fun `store asset instruction committed`() {
-        val assetDefinition = DefinitionId("xor", "wonderland")
+        val assetDefinition = DefinitionId("xor", DEFAULT_DOMAIN_NAME)
         val assetId = AssetId(assetDefinition, ALICE_ACCOUNT_ID)
         val pair1 = "key1" to "bar".asValue()
         val pair2 = "key2" to true.asValue()
@@ -67,7 +68,7 @@ class InstructionsTest {
 
         Assertions.assertDoesNotThrow {
             client.sendTransactionAsync {
-                account("alice", "wonderland")
+                account(ALICE_ACCOUNT_ID)
                 instruction { registerAsset(assetDefinition, AssetValueType.Store()) }
                 instruction { storeAsset(assetId, pair1.first, pair1.second) }
                 instruction { storeAsset(assetId, pair2.first, pair2.second) }
@@ -88,9 +89,57 @@ class InstructionsTest {
         assertEquals(assetId.definitionId.domainName, asset.id.definitionId.domainName)
         when (val value = asset.value) {
             is AssetValue.Store -> {
-                assertEquals(pair1.second.string, (value.metadata.map[pair1.first] as? Value.String)?.string)
-                assertEquals(pair2.second.bool, (value.metadata.map[pair2.first] as? Value.Bool)?.bool)
-                assertEquals(pair3.second.u32, (value.metadata.map[pair3.first] as? Value.U32)?.u32)
+                assertEquals(pair1.second.string, value.metadata.map[pair1.first]?.cast<Value.String>()?.string)
+                assertEquals(pair2.second.bool, value.metadata.map[pair2.first]?.cast<Value.Bool>()?.bool)
+                assertEquals(pair3.second.u32, (value.metadata.map[pair3.first]?.cast<Value.U32>())?.u32)
+            }
+            else -> fail("Expected result asset value has type `AssetValue.Store`, but it was `${asset.value::class.simpleName}`")
+        }
+    }
+
+    @Test
+    fun `grant access to asset key-value committed`() {
+        val assetDefinition = DefinitionId("xor", DEFAULT_DOMAIN_NAME)
+        val aliceAssetId = AssetId(assetDefinition, ALICE_ACCOUNT_ID)
+        val bobAccountId = AccountId("bob", DEFAULT_DOMAIN_NAME)
+        val bobKeypair = generateKeyPair()
+
+        // transaction from behalf of Alice. Alice gives permission to Bob to set key-value Asset.Store in her account
+        Assertions.assertDoesNotThrow {
+            client.sendTransactionAsync {
+                account(ALICE_ACCOUNT_ID)
+                // register asset with type store
+                instruction { registerAsset(assetDefinition, AssetValueType.Store()) }
+                // register Bob's account
+                instruction { registerAccount(bobAccountId, mutableListOf(bobKeypair.public.toIrohaPublicKey())) }
+                // grant by Alice to Bob permissions to set key value in Asset.Store
+                instruction { grantPermissionsToKeyValueAsset(aliceAssetId, bobAccountId) }
+                buildSigned(DEFAULT_KEYPAIR)
+            }.get(10, TimeUnit.SECONDS)
+        }
+
+        // transaction from behalf of Bob. He tries to set key-value Asset.Store to the Alice account
+        Assertions.assertDoesNotThrow {
+            client.sendTransactionAsync {
+                account(bobAccountId)
+                instruction { storeAsset(aliceAssetId, "foo", "bar".asValue()) }
+                buildSigned(bobKeypair)
+            }.get(10, TimeUnit.SECONDS)
+        }
+
+        val asset = client.sendQuery(::assetExtractor) {
+            accountId = ALICE_ACCOUNT_ID
+            query {
+                findAssetById(aliceAssetId)
+            }
+            buildSigned(DEFAULT_KEYPAIR)
+        }
+
+        assertEquals(aliceAssetId.definitionId.name, asset.id.definitionId.name)
+        assertEquals(aliceAssetId.definitionId.domainName, asset.id.definitionId.domainName)
+        when (val value = asset.value) {
+            is AssetValue.Store -> {
+                assertEquals("bar", (value.metadata.map["foo"]?.cast<Value.String>())?.string)
             }
             else -> fail("Expected result asset value has type `AssetValue.Store`, but it was `${asset.value::class.simpleName}`")
         }
