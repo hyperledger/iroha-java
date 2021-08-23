d1d512b592cf792e019743e1c065f5556a963a93
rkharisov <50765419+rkharisov@users.noreply.github.com>
2021-08-23 11:09:57 +0300

    Changed returned type for methods to create public and private keys from hex (#107)
    Signed-off-by: rkharisov <rinat@soramitsu.co.jp>

--- modules/client/src/main/kotlin/jp/co/soramitsu/iroha2/Extractors.kt
+++ modules/client/src/main/kotlin/jp/co/soramitsu/iroha2/Extractors.kt
@@ -6,24 +6,34 @@ import jp.co.soramitsu.iroha2.generated.datamodel.account.Account
 import jp.co.soramitsu.iroha2.generated.datamodel.asset.Asset
 import jp.co.soramitsu.iroha2.generated.datamodel.query.QueryResult
 
-fun asIs(queryResult: QueryResult): QueryResult = queryResult
+interface ResultExtractor<T> {
+    fun extract(result: QueryResult): T
+}
 
-fun assetExtractor(result: QueryResult): Asset {
-    return when (val value = result.value) {
-        is Value.Identifiable -> when (val box = value.identifiableBox) {
-            is IdentifiableBox.Asset -> box.asset
+object AsIs : ResultExtractor<QueryResult> {
+    override fun extract(result: QueryResult) = result
+}
+
+object AssetExtractor : ResultExtractor<Asset> {
+    override fun extract(result: QueryResult): Asset {
+        return when (val value = result.value) {
+            is Value.Identifiable -> when (val box = value.identifiableBox) {
+                is IdentifiableBox.Asset -> box.asset
+                else -> throw QueryPayloadExtractionException()
+            }
             else -> throw QueryPayloadExtractionException()
         }
-        else -> throw QueryPayloadExtractionException()
     }
 }
 
-fun accountExtractor(result: QueryResult): Account {
-    return when (val value = result.value) {
-        is Value.Identifiable -> when (val box = value.identifiableBox) {
-            is IdentifiableBox.Account -> box.account
+object AccountExtractor : ResultExtractor<Account> {
+    override fun extract(result: QueryResult): Account {
+        return when (val value = result.value) {
+            is Value.Identifiable -> when (val box = value.identifiableBox) {
+                is IdentifiableBox.Account -> box.account
+                else -> throw QueryPayloadExtractionException()
+            }
             else -> throw QueryPayloadExtractionException()
         }
-        else -> throw QueryPayloadExtractionException()
     }
 }
