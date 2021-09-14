d02134b32f9bcb1c407b616f328a093fd26d4d05
rkharisov <50765419+rkharisov@users.noreply.github.com>
2021-09-14 13:09:22 +0300

    Altered coroutine scope from Global to local for Iroha2Client (#141)
    Signed-off-by: rkharisov <rinat@soramitsu.co.jp>

--- modules/client/src/test/kotlin/jp/co/soramitsu/iroha2/util/CryptoTest.kt
+++ modules/client/src/test/kotlin/jp/co/soramitsu/iroha2/util/CryptoTest.kt
@@ -7,7 +7,8 @@ import jp.co.soramitsu.iroha2.hex
 import jp.co.soramitsu.iroha2.keyPairFromHex
 import jp.co.soramitsu.iroha2.sign
 import jp.co.soramitsu.iroha2.verify
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import org.bouncycastle.util.encoders.Hex
@@ -21,7 +22,7 @@ class CryptoTest {
     @Test
     fun `generating key pairs is thread safe`() {
         val iterations = 1000
-        val futureResults = generateSequence { GlobalScope.async { generateKeyPair() } }
+        val futureResults = generateSequence { CoroutineScope(Dispatchers.Default).async { generateKeyPair() } }
             .take(iterations).toSet()
 
         class ByteArrayWrapper(private val byteArray: ByteArray) {
@@ -78,23 +79,4 @@ class CryptoTest {
             assertTrue { restoredKeyPair.public.verify(signature, message) }
         }
     }
-
-    // @Test
-    // fun `transaction signature is same as expected`() {
-    //     val keyPair = keyPairFromHex(
-    //         publicKeyHex = "5b4dfe6632ee7e3627e9c2052339314a14ae8f565fd2558711caf6273c27cfe7",
-    //         privateKeyHex = "0c7464df2a1fb6b0f78fb1f2c9c01a436c0ccd76be11a90d835080145159d928"
-    //     )
-    //     val expectedSignature = "f2139d3c91bb605edbc783cc900abdd5b81b687c10d2e2827b23e1c7626c3a15b3adaded97f65a2fdbde3839bda9a2b2ca889f18a8d9ac8b2a9cab9149a68d0a"
-    //     val versionedTx = TransactionBuilder.builder()
-    //         .account(ALICE_ACCOUNT_ID)
-    //         .creationTime(0)
-    //         .timeToLive(0)
-    //         .buildSigned(keyPair) as VersionedTransaction.V1
-    //
-    //     val signatures = versionedTx._VersionedTransactionV1.transaction.signatures
-    //     assertEquals(1, signatures.size)
-    //     val signature = signatures.first()
-    //     assertContentEquals(hex(expectedSignature), signature.signature.toUByteArray().toByteArray())
-    // }
 }
