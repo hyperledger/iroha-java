2f23364a51067f25fb585c108a1a4864158c6dd3
Andrey Kostyuchenko <andreykostyuchenko94@gmail.com>
2021-10-06 13:33:34 +0500

    Iroha-121: implement all queries in QueryBuilder and add tests for them (#156)
    * iroha-121: WIP implement queries in QueryBuilder
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: merge with develop
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: lint changes
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: QueriesTest and account creation in genesis
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: lint changes
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: minor changes
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: queries findAssetsByName, findAllAssets, findAccountsByDomainName
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: lint changes
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: merge with develop
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: minor changes
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: WIP
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: queries findAssetKeyValueByIdAndKey, findAssetQuantityById, findAssetsByDomainNameAndAssetDefinitionId
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: lint changes
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: find assets by account id test changes
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: WIP
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: implement remaining queries in QueryBuilder
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: lint changes
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    * iroha-121: minot fix
    
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>
    
    Co-authored-by: akostiucenko <kostiuchenko@soramitsu.co.jp>

--- modules/client/src/test/kotlin/jp/co/soramitsu/iroha2/QueriesTest.kt
+++ modules/client/src/test/kotlin/jp/co/soramitsu/iroha2/QueriesTest.kt
@@ -3,18 +3,29 @@ package jp.co.soramitsu.iroha2
 import jp.co.soramitsu.iroha2.engine.ALICE_ACCOUNT_ID
 import jp.co.soramitsu.iroha2.engine.ALICE_ACCOUNT_NAME
 import jp.co.soramitsu.iroha2.engine.ALICE_KEYPAIR
+import jp.co.soramitsu.iroha2.engine.AliceHas100XorAndPermissionToBurn
+import jp.co.soramitsu.iroha2.engine.DEFAULT_ASSET_DEFINITION_ID
 import jp.co.soramitsu.iroha2.engine.DEFAULT_DOMAIN_NAME
 import jp.co.soramitsu.iroha2.engine.IrohaRunnerExtension
-import jp.co.soramitsu.iroha2.engine.MultipleAssets
 import jp.co.soramitsu.iroha2.engine.NewAccountWithMetadata
+import jp.co.soramitsu.iroha2.engine.NewDomain
+import jp.co.soramitsu.iroha2.engine.StoreAssetWithMetadata
 import jp.co.soramitsu.iroha2.engine.WithIroha
+import jp.co.soramitsu.iroha2.engine.XorAndValAssets
+import jp.co.soramitsu.iroha2.generated.datamodel.IdBox
+import jp.co.soramitsu.iroha2.generated.datamodel.Value
+import jp.co.soramitsu.iroha2.generated.datamodel.asset.AssetValueType
 import jp.co.soramitsu.iroha2.generated.datamodel.asset.Id
+import jp.co.soramitsu.iroha2.generated.datamodel.transaction.TransactionValue
+import jp.co.soramitsu.iroha2.generated.datamodel.transaction.VersionedTransaction
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
+import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 
 @Execution(ExecutionMode.CONCURRENT)
@@ -81,7 +92,7 @@ class QueriesTest {
     }
 
     @Test
-    @WithIroha(MultipleAssets::class)
+    @WithIroha(XorAndValAssets::class)
     fun `find all assets`(): Unit = runBlocking {
         QueryBuilder.findAllAssets()
             .account(ALICE_ACCOUNT_ID)
@@ -89,26 +100,26 @@ class QueriesTest {
             .let { query ->
                 client.sendQuery(query)
             }.also { assets ->
-                assert(assets.any { it.id.definitionId == MultipleAssets.XOR_DEFINITION_ID })
-                assert(assets.any { it.id.definitionId == MultipleAssets.VAL_DEFINITION_ID })
+                assert(assets.any { it.id.definitionId == XorAndValAssets.XOR_DEFINITION_ID })
+                assert(assets.any { it.id.definitionId == XorAndValAssets.VAL_DEFINITION_ID })
             }
     }
 
     @Test
-    @WithIroha(MultipleAssets::class)
+    @WithIroha(XorAndValAssets::class)
     fun `find assets by name`(): Unit = runBlocking {
-        QueryBuilder.findAssetsByName(MultipleAssets.XOR_DEFINITION_ID.name)
+        QueryBuilder.findAssetsByName(XorAndValAssets.XOR_DEFINITION_ID.name)
             .account(ALICE_ACCOUNT_ID)
             .buildSigned(ALICE_KEYPAIR)
             .let { query ->
                 client.sendQuery(query)
             }.also { assets ->
-                assert(assets.all { it.id.definitionId.name == MultipleAssets.XOR_DEFINITION_ID.name })
+                assert(assets.all { it.id.definitionId.name == XorAndValAssets.XOR_DEFINITION_ID.name })
             }
     }
 
     @Test
-    @WithIroha(MultipleAssets::class)
+    @WithIroha(XorAndValAssets::class)
     fun `find assets by account id`(): Unit = runBlocking {
         QueryBuilder.findAssetsByAccountId(ALICE_ACCOUNT_ID)
             .account(ALICE_ACCOUNT_ID)
@@ -117,55 +128,165 @@ class QueriesTest {
                 client.sendQuery(query)
             }.also { assets ->
                 assert(assets.all { it.id.accountId == ALICE_ACCOUNT_ID })
-                assert(assets.any { it.id.definitionId == MultipleAssets.XOR_DEFINITION_ID })
-                assert(assets.any { it.id.definitionId == MultipleAssets.VAL_DEFINITION_ID })
+                assert(assets.any { it.id.definitionId == XorAndValAssets.XOR_DEFINITION_ID })
+                assert(assets.any { it.id.definitionId == XorAndValAssets.VAL_DEFINITION_ID })
             }
     }
 
     @Test
-    @WithIroha(MultipleAssets::class)
+    @WithIroha(XorAndValAssets::class)
     fun `find assets by domain name and asset definition id`(): Unit = runBlocking {
         QueryBuilder.findAssetsByDomainNameAndAssetDefinitionId(
             DEFAULT_DOMAIN_NAME,
-            MultipleAssets.XOR_DEFINITION_ID
+            XorAndValAssets.XOR_DEFINITION_ID
         )
             .account(ALICE_ACCOUNT_ID)
             .buildSigned(ALICE_KEYPAIR)
             .let { query ->
                 client.sendQuery(query)
             }.also { assets ->
-                assert(assets.all { it.id.definitionId == MultipleAssets.XOR_DEFINITION_ID })
+                assert(assets.all { it.id.definitionId == XorAndValAssets.XOR_DEFINITION_ID })
                 assert(assets.all { it.id.accountId.domainName == DEFAULT_DOMAIN_NAME })
             }
     }
 
     @Test
-    @WithIroha(MultipleAssets::class)
+    @WithIroha(XorAndValAssets::class)
     fun `find asset quantity by id`(): Unit = runBlocking {
-        val assetId = Id(MultipleAssets.XOR_DEFINITION_ID, ALICE_ACCOUNT_ID)
+        val assetId = Id(XorAndValAssets.XOR_DEFINITION_ID, ALICE_ACCOUNT_ID)
         QueryBuilder.findAssetQuantityById(assetId)
             .account(ALICE_ACCOUNT_ID)
             .buildSigned(ALICE_KEYPAIR)
             .let { query ->
                 client.sendQuery(query)
             }.also { quantity ->
-                assert(quantity == MultipleAssets.XOR_QUANTITY)
+                assert(quantity == XorAndValAssets.XOR_QUANTITY)
             }
     }
 
     @Test
-    @WithIroha(MultipleAssets::class)
+    @WithIroha(StoreAssetWithMetadata::class)
     fun `find asset key value by id and key`(): Unit = runBlocking {
         QueryBuilder.findAssetKeyValueByIdAndKey(
-            MultipleAssets.FOO_ASSET_ID,
-            MultipleAssets.FOO_ASSET_KEY
+            StoreAssetWithMetadata.ASSET_ID,
+            StoreAssetWithMetadata.ASSET_KEY
         )
             .account(ALICE_ACCOUNT_ID)
             .buildSigned(ALICE_KEYPAIR)
             .let { query ->
                 client.sendQuery(query)
             }.also { value ->
-                assert(value == MultipleAssets.FOO_ASSET_VALUE)
+                assert(value == StoreAssetWithMetadata.ASSET_VALUE)
+            }
+    }
+
+    @Test
+    @WithIroha(StoreAssetWithMetadata::class)
+    fun `find asset definition key value by id and key`(): Unit = runBlocking {
+        QueryBuilder.findAssetDefinitionKeyValueByIdAndKey(
+            StoreAssetWithMetadata.DEFINITION_ID,
+            StoreAssetWithMetadata.ASSET_KEY
+        )
+            .account(ALICE_ACCOUNT_ID)
+            .buildSigned(ALICE_KEYPAIR)
+            .let { query ->
+                client.sendQuery(query)
+            }.also {
+                assert(it == StoreAssetWithMetadata.ASSET_VALUE)
+            }
+    }
+
+    @Test
+    @WithIroha(NewDomain::class)
+    fun `find all domains`(): Unit = runBlocking {
+        QueryBuilder.findAllDomains()
+            .account(ALICE_ACCOUNT_ID)
+            .buildSigned(ALICE_KEYPAIR)
+            .let { query ->
+                client.sendQuery(query)
+            }.also { domains ->
+                assert(domains.any { it.name == DEFAULT_DOMAIN_NAME })
+                assert(domains.any { it.name == NewDomain.DOMAIN_NAME })
+            }
+    }
+
+    @Test
+    @WithIroha
+    fun `find domain by name`(): Unit = runBlocking {
+        QueryBuilder.findDomainByName(DEFAULT_DOMAIN_NAME)
+            .account(ALICE_ACCOUNT_ID)
+            .buildSigned(ALICE_KEYPAIR)
+            .let { query ->
+                client.sendQuery(query)
+            }.also { domain ->
+                assert(domain.name == DEFAULT_DOMAIN_NAME)
+            }
+    }
+
+    @Test
+    @WithIroha
+    fun `find all peers`(): Unit = runBlocking {
+        QueryBuilder.findAllPeers()
+            .account(ALICE_ACCOUNT_ID)
+            .buildSigned(ALICE_KEYPAIR)
+            .let { query ->
+                client.sendQuery(query)
+            }.also { peers ->
+                assert(peers.isNotEmpty())
+            }
+    }
+
+    @Test
+    @WithIroha
+    fun `find transactions by account id`(): Unit = runBlocking {
+        client.sendTransaction {
+            account(ALICE_ACCOUNT_ID)
+            registerAsset(DEFAULT_ASSET_DEFINITION_ID, AssetValueType.Quantity())
+            buildSigned(ALICE_KEYPAIR)
+        }.also {
+            Assertions.assertDoesNotThrow {
+                it.get(10, TimeUnit.SECONDS)
+            }
+        }
+
+        QueryBuilder.findTransactionsByAccountId(ALICE_ACCOUNT_ID)
+            .account(ALICE_ACCOUNT_ID)
+            .buildSigned(ALICE_KEYPAIR)
+            .let { query ->
+                client.sendQuery(query)
+            }.also { txValues ->
+                txValues.all { value ->
+                    value.cast<TransactionValue.Transaction>()
+                        .versionedTransaction
+                        .cast<VersionedTransaction.V1>()
+                        ._VersionedTransactionV1
+                        .transaction
+                        .payload
+                        .accountId == ALICE_ACCOUNT_ID
+                }.also {
+                    assert(it)
+                }
+            }
+    }
+
+    @Test
+    @WithIroha(AliceHas100XorAndPermissionToBurn::class)
+    fun `find permission tokens by account id`(): Unit = runBlocking {
+        QueryBuilder.findPermissionTokensByAccountId(ALICE_ACCOUNT_ID)
+            .account(ALICE_ACCOUNT_ID)
+            .buildSigned(ALICE_KEYPAIR)
+            .let { query ->
+                client.sendQuery(query)
+            }.also { tokens ->
+                tokens.any {
+                    it.params[ASSET_DEFINITION_PARAM_NAME]
+                        ?.cast<Value.Id>()
+                        ?.idBox
+                        ?.cast<IdBox.AssetDefinitionId>()
+                        ?.definitionId == DEFAULT_ASSET_DEFINITION_ID
+                }.also {
+                    assert(it)
+                }
             }
     }
 }
