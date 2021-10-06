5a2dce7f654d1f525cc41bff28f46b21db780826
akostiucenko <kostiuchenko@soramitsu.co.jp>
2021-10-06 14:12:10 +0500

    iroha-151: update docker image name in test containers
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>

--- modules/testcontainers/src/main/kotlin/jp/co/soramitsu/iroha2/testcontainers/IrohaContainer.kt
+++ modules/testcontainers/src/main/kotlin/jp/co/soramitsu/iroha2/testcontainers/IrohaContainer.kt
@@ -102,9 +102,9 @@ open class IrohaContainer(
             "IROHA_PRIVATE_KEY" to """{"digest_function": "ed25519", "payload": "9ac47abf59b356e0bd7dcbbbb4dec080e302156a48ca907e47cb6aea1d32719e7233bfc89dcbd68c19fde6ce6158225298ec1131b6a130d1aeb454c1ab5183c0"}"""
         val ENV_SUMERAGI_TRUSTED_PEERS =
             "SUMERAGI_TRUSTED_PEERS" to """[{"address":"$P2P_URL", "public_key": "$IROHA_ROOT_PUBLIC_KEY"}]"""
-        const val DEFAULT_IMAGE_TAG = "iroha2-dev"
+        const val DEFAULT_IMAGE_TAG = "dev"
 
-        const val IMAGE_NAME = "iroha1/iroha"
+        const val IMAGE_NAME = "hyperledger/iroha2"
         const val DEFAULT_GENESIS_FILE_NAME = "genesis.json"
         const val PEER_START_COMMAND = "./iroha_cli --submit-genesis --genesis-path $DEFAULT_GENESIS_FILE_NAME"
         val CONTAINER_STARTUP_TIMEOUT: Duration = Duration.ofSeconds(60)
