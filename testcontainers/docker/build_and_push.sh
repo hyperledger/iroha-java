a6ef0613430ae5c695ac8a9e627324f42c260b87
Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
2021-10-25 13:12:47 +0300

    update to iroha:1.3.0-rc.1
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

--- testcontainers/docker/build_and_push.sh
+++ testcontainers/docker/build_and_push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export TAG=ghcr.io/hyperledger/iroha:df8b9258ff546ebf7dcdbe6ca2f52ac82f94922138649fe9fc875558b0c3e715-debug
+export TAG=ghcr.io/hyperledger/iroha:1.3.0-rc.1
 
 docker build -t $TAG --compress --squash .
 docker push $TAG
