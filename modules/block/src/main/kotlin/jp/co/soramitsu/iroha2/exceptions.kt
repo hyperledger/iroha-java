4a989abb2b8107ba8a088fc2a69ec6e867599b6a
Ekaterina Mekhnetsova <mekkatya@gmail.com>
2022-06-13 11:07:54 +0300

    [documentation] #232: Update documentation in the block module
    Signed-off-by: Ekaterina Mekhnetsova <mekkatya@gmail.com>

--- modules/block/src/main/kotlin/jp/co/soramitsu/iroha2/exceptions.kt
+++ modules/block/src/main/kotlin/jp/co/soramitsu/iroha2/exceptions.kt
@@ -1,5 +1,15 @@
 package jp.co.soramitsu.iroha2
 
+/**
+ * Throw if there is an issue with deserialization.
+ * 
+ * @param message The explanation of the issue
+ */
 class DeserializationException(message: String) : RuntimeException(message)
 
+/**
+ * Throw if there is an issue with serialization
+ * 
+ * @param message The explanation of the issue
+ */
 class SerializationException(message: String) : RuntimeException(message)
