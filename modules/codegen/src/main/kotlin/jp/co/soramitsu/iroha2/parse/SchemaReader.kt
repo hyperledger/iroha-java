cc29b54ccf1537973e4e994f9477157baa1b0c9f
akostiucenko <kostiuchenko@soramitsu.co.jp>
2022-06-20 18:59:05 +0500

    iroha-228: WIP
    Signed-off-by: akostiucenko <kostiuchenko@soramitsu.co.jp>

--- modules/codegen/src/main/kotlin/jp/co/soramitsu/iroha2/parse/SchemaReader.kt
+++ modules/codegen/src/main/kotlin/jp/co/soramitsu/iroha2/parse/SchemaReader.kt
@@ -6,9 +6,11 @@ import kotlin.streams.toList
 
 class SchemaReader {
     companion object {
-        const val SCHEMA_FILTER_TY = "ty:"
-        val dataFilterPattern = "data::filters::(.+)<".toRegex()
-        val dataFilterRegex = "\"iroha_data_model::events::data::filters(.+)\"".toRegex()
+        private const val SCHEMA_FILTER_TY = "ty:"
+        private const val REPEATED_PATTERN = "iroha_data_model::(.+)::"
+
+        private val dataFilterRegex = "data::filters::(.+)<".toRegex()
+        private val eventDataFilterRegex = "\"iroha_data_model::events::data::filters(.+)\"".toRegex()
     }
 
     private val repeated = mutableMapOf<String, Int>()
@@ -19,9 +21,13 @@ class SchemaReader {
         val lines = resource.bufferedReader().lines().toList()
 
         lines.forEach { countRepeated(it) }
-        repeated.entries.removeIf { it.value <= 1 }
+        repeated.entries.removeIf { it.value <= 2 } // todo <= 1
 
-        lines.forEach { sb.appendLine(parseLine(it)) }
+        val repeatedRegex = repeated.keys
+            .map { it.split("::").last().replace("\": {") }
+            .let { classes -> "(${classes.joinToString("|")})" }
+            .let { classes -> "$REPEATED_PATTERN$classes\\W".toRegex() }
+        lines.forEach { sb.appendLine(parseLine(it, repeatedRegex)) }
 
         return ObjectMapper().readValue(
             sb.toString(),
@@ -29,26 +35,43 @@ class SchemaReader {
         )
     }
 
-    private fun parseLine(line: String): String {
-        if (line.contains(dataFilterPattern)) {
-            var tmpLine = line.substringBeforeLast("\"").replace("\"", "").trim()
+    private fun parseLine(line: String, repeatedRegex: Regex): String {
+        if (line.contains(dataFilterRegex)) {
+            var tmpLine = line.substringBeforeLast("\"").replace("\"").trim()
             if (tmpLine.startsWith(SCHEMA_FILTER_TY)) {
                 tmpLine = tmpLine.substring(SCHEMA_FILTER_TY.length + 1)
             }
             val newFilterName = getFilterEventName(tmpLine)
             val newLine = "\"" + tmpLine.substringBefore(getDelimiter(line)) + newFilterName + "\""
-            return line.replace(dataFilterRegex, newLine)
+
+            return line.replace(eventDataFilterRegex, newLine)
+        } else if (line.contains(repeatedRegex)) {
+            val extracted = repeatedRegex.find(line)?.value
+                ?: return line
+
+            val lineParts = extracted.split("::")
+            val className = lineParts[lineParts.size - 1] // todo safely
+            val prefix = lineParts[lineParts.size - 2]
+                .split('_').joinToString("") { part ->
+                    part.replaceFirstChar { it.uppercaseChar() }
+                }
+
+            return extracted.replace(
+                className, "$prefix$className"
+            ).let { modified ->
+                line.replace(extracted, modified)
+            }
         }
 
         return line
     }
 
     private fun countRepeated(line: String) {
-        if (line.contains("\"iroha_data_model::(.+)::(.+)\": \\{".toRegex())) {
+        if (line.contains("\"iroha_data_model::(.+)::(.+)\": \\{".toRegex())) { // todo
             val parts = line.split("::")
             if (parts.size <= 1) return
 
-            val key = "::${parts.last()}"
+            val key = "::${parts.last()}".replace(">")
             val counter = repeated.getOrDefault(key, 0)
             repeated[key] = counter + 1
         }
@@ -62,15 +85,17 @@ class SchemaReader {
         if (name.contains("data::filters")) {
             val newName = name.substringBefore("<").substringAfterLast("::")
             return when {
-                !name.contains("<") -> newName.replace(">", "")
+                !name.contains("<") -> newName.replace(">")
                 else -> newName + getFilterEventName(name.substringAfter("<"))
             }
         }
-        val tokens = name.replace(">", "").split("::")
+        val tokens = name.replace(">").split("::")
         val sb = StringBuilder()
         for (i in 1 until tokens.size) {
             sb.append(tokens[i].replaceFirstChar { tokens[i].first().uppercaseChar() })
         }
         return sb.toString()
     }
+
+    private fun String.replace(oldValue: String) = this.replace(oldValue, "")
 }
