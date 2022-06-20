d882a57ce07c8c71311c177d2e8cd90e6945d8e2
Ekaterina Mekhnetsova <mekkatya@gmail.com>
2022-06-20 14:32:39 +0300

    [documentation] #232: Update documentation in the admin-client module (#237)
    Signed-off-by: Ekaterina Mekhnetsova <mekkatya@gmail.com>

--- modules/admin-client/src/main/kotlin/jp/co/soramitsu/iroha2/PeerStatus.kt
+++ modules/admin-client/src/main/kotlin/jp/co/soramitsu/iroha2/PeerStatus.kt
@@ -2,6 +2,15 @@ package jp.co.soramitsu.iroha2
 
 import java.time.Duration
 
+/**
+ * The status of a peer
+ * @property peers The number of connected peers, except for the reporting peer itself
+ * @property blocks The number of commited blocks
+ * @property txs_accepted Total number of accepted transactions
+ * @property txs_rejected Total number of rejected transactions
+ * @property uptime Uptime with nanosecond precision since creation of the genesis block
+ * @property view_changes The number of view changes in the current round
+ */
 data class PeerStatus(
     val peers: Int,
     val blocks: Long,
