1600790f5a7911a6ae4fea1d83cea2b83918d6e5
Timur Guskov <guskov@soramitsu.co.jp>
2022-06-30 10:33:06 +0300

    feature: added support for environment variables to set image name, tag and pull policy
    Signed-off-by: Timur Guskov <guskov@soramitsu.co.jp>

--- modules/testcontainers/src/main/kotlin/jp/co/soramitsu/iroha2/testcontainers/IrohaContainer.kt
+++ modules/testcontainers/src/main/kotlin/jp/co/soramitsu/iroha2/testcontainers/IrohaContainer.kt
@@ -49,7 +49,12 @@ open class IrohaContainer : GenericContainer<IrohaContainer> {
                 DEFAULT_CONFIG_FILE_NAME
             )
             .withCommand(PEER_START_COMMAND)
-            .withImagePullPolicy(PullPolicy.ageBased(Duration.ofMinutes(10)))
+            .withImagePullPolicy(
+                if (IMAGE_PULL_POLICY == "aged")
+                    PullPolicy.ageBased(Duration.ofMinutes(10))
+                else
+                    PullPolicy.defaultPolicy()
+            )
             .waitingFor(
                 // await genesis was applied and seen in status
                 HttpWaitStrategy()
@@ -97,8 +102,9 @@ open class IrohaContainer : GenericContainer<IrohaContainer> {
 
     companion object {
         const val NETWORK_ALIAS = "iroha"
-        const val DEFAULT_IMAGE_TAG = "stable"
-        const val IMAGE_NAME = "hyperledger/iroha2"
+        val DEFAULT_IMAGE_TAG = System.getenv("IROHA_JAVA_IMAGE_TAG") ?: "stable"
+        val IMAGE_NAME = System.getenv("IROHA_JAVA_IMAGE_NAME") ?: "hyperledger/iroha2"
+        val IMAGE_PULL_POLICY = System.getenv("IROHA_JAVA_IMAGE_PULL_POLICY") ?: "aged"
         const val DEFAULT_GENESIS_FILE_NAME = "genesis.json"
         const val DEFAULT_CONFIG_FILE_NAME = "config.json"
         const val PEER_START_COMMAND = "./iroha --submit-genesis"
