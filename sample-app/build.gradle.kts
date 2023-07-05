e72f027d56774bfbaca18ab399429f7821f1d48e
Marin Veršić <marin.versic101@gmail.com>
2023-07-05 13:18:49 +0200

    bump dependency versions
    Signed-off-by: Marin Veršić <marin.versic101@gmail.com>

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.8.22"
     application
 }
 
@@ -16,16 +16,16 @@ repositories {
 dependencies {
     val iroha2Ver by System.getProperties()
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.7.2")
 
     api("com.github.hyperledger.iroha-java:admin-client:$iroha2Ver")
     implementation("com.github.hyperledger.iroha-java:model:$iroha2Ver")
     implementation("com.github.hyperledger.iroha-java:block:$iroha2Ver")
 
     implementation("net.i2p.crypto:eddsa:0.3.0")
-    implementation("org.bouncycastle:bcprov-jdk15on:1.65")
-    implementation("com.github.multiformats:java-multihash:1.3.0")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+    implementation("com.github.multiformats:java-multihash:1.3.1")
 }
 
 tasks.test {
